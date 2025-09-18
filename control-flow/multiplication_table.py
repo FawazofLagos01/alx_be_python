01f15e6fc6e97b3e7c856afc90877e762bdae8ce
FawazofLagos01 <aybest1245@gmail.com>
2025-09-18 09:00:11 +0100

    Add basic number multiplication

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,5 @@
 number = int(input("Enter a number to see its multiplication table: "))
-X = number
-for Y in range (1, 11):
-    product = X * Y
-    Z = product
-    print(f"{X} * {Y} = {Z}")
+for i in range (1, 11):
+    product = number * i
+    print(f"{number} * {i} = {product}")
 print()
