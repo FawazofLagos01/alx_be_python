94e48e18a8f6297dc12788f10d1a449158e97364
FawazofLagos01 <aybest1245@gmail.com>
2025-09-18 08:52:52 +0100

    Create a number multiplication value

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number to see its multiplication table: "))
+X = number
+for Y in range (1, 11):
+    product = X * Y
+    Z = product
+    print(f"{X} * {Y} = {Z}")
+print()
