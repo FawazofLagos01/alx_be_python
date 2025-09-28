6bd302bdd0949b891b6a9347ca65cb9abb58412e
FawazofLagos01 <aybest1245@gmail.com>
2025-09-28 07:56:19 +0100

    Add Temperature conversion tool between two scales; Celsius and Fahrenheit

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -24,7 +24,7 @@ def main():
             print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
 
     except ValueError:
-        print("Invalid temperature. Please enter a nueric value.")
+        print("Invalid temperature. Please enter a numeric value.")
 
 if __name__ == "__main__":
     main()
