6656b8b634ff0be09735b554f2e65009ae49b673
FawazofLagos01 <aybest1245@gmail.com>
2025-10-01 18:42:03 +0100

    creating a simple bank account class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+class BankAccount():
+    def __init__(self, initial_balance=0):
+        """Initialize account with an optional starting balance."""
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        """Deposit a specific amount"""
+        if amount > 0:
+            self.account_balance += amount
+
+    def withdraw(self, amount):
+        """Withdraw money if Account balance is greater than or equal to withdrawal amount."""
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
+    
+    def display_balance(self):
+        """print the current balance in a user-friendly format."""
+        print(f"Current Balance: ${self.account_balance}")
+    
